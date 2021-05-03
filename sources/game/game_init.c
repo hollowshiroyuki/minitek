b18eb93b4998c1bd267ca6d1ba5c0d2b2ce289f1
Corentin Vidal <corentin.vidal@epitech.eu>
2021-05-03 18:54:48 +0200

    Fix: minor fixes on furniture

--- sources/game/game_init.c
+++ sources/game/game_init.c
@@ -10,17 +10,15 @@
 #include "scenes.h"
 #include "universe.h"
 #include "menus/title_menu.h"
+#include "entities/workbench.h"
 
 void game_init(engine_t *engine)
 {
     game_data_t *data = &engine->game_data;
+    entity_t *workbench = workbench_create();
 
     screen_init(&data->screen, &engine->assets, engine->window);
     data->input = input_create();
     data->universe = universe_create((sfVector2i){256, 256}, data->input, 42);
     universe_set_menu(data->universe, title_menu_create());
-    for (int i = 0; i < 0; i++)
-        inventory_add(data->universe->player->mob.pla.inventory, tool_item_create((tool_type_t *)(&sword_type), 1));
-    //for (int i = 0; i < 10; i++)
-    //    inventory_add(data->universe->player->mob.pla.inventory, tool_item_create((tool_type_t *)(&sword_type), 3));
 }
