ad7c29dadfe21d1440e720a41527a5e265abe64a
Corentin Vidal <corentin.vidal@epitech.eu>
2021-05-04 09:40:22 +0200

    Fix: Now also copy item list else we'll unlink them in the original list

--- sources/recipes/recipe_list_copy.c
+++ sources/recipes/recipe_list_copy.c
@@ -9,10 +9,11 @@
 #include <stdlib.h>
 #include "recipes/craft.h"
 
-static void append(recipe_t **list, recipe_t *new)
+static void append(recipe_t **list, recipe_t *new, item_t *item)
 {
     recipe_t *tmp = *list;
 
+    new->result = item;
     if (!tmp) {
         *list = new;
         return;
@@ -20,18 +21,23 @@ static void append(recipe_t **list, recipe_t *new)
     while (tmp->next)
         tmp = tmp->next;
     tmp->next = new;
+    tmp->result->next = new->result;
 }
 
 recipe_t *recipe_list_copy(recipe_t *list)
 {
-    recipe_t *tmp;
+    recipe_t *r_tmp;
+    item_t *i_tmp;
     recipe_t *res = 0;
 
     while (list) {
-        tmp = malloc(sizeof(recipe_t));
-        *tmp = *list;
-        tmp->next = 0;
-        append(&res, tmp);
+        r_tmp = malloc(sizeof(recipe_t));
+        i_tmp = malloc(sizeof(item_t));
+        *r_tmp = *list;
+        *i_tmp = *list->result;
+        i_tmp->next = 0;
+        r_tmp->next = 0;
+        append(&res, r_tmp, i_tmp);
         list = list->next;
     }
     return (res);
