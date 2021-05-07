e9748422b0557e2dc674b0397fa8fbda285d4f96
Corentin Vidal <corentin.vidal@epitech.eu>
2021-05-07 13:34:41 +0200

    Add: Player can't pick up items if full

--- sources/entities/mob/player/player_touch_item.c
+++ sources/entities/mob/player/player_touch_item.c
@@ -6,10 +6,20 @@
 */
 
 #include <stdio.h>
+#include "items_id.h"
 #include "entities/entity.h"
 
 void player_touch_item(entity_t *self, entity_t *item)
 {
+    inventory_t *inv = self->mob.pla.inventory;
+    bool full = item_list_size(inv->items) >= inv->capacity;
+    item_t *has;
+
+    if (full && item->itm.item->id != I_RESOURCE)
+        return;
+    has = inventory_find_resource(inv, item->itm.item->res.resource);
+    if (full && !has)
+        return;
     item_entity_take(item, self);
     inventory_add(self->mob.pla.inventory, item->itm.item);
 }
