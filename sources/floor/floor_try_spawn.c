d32090c97609fc38b558724f32d2b90e9384a557
Corentin Vidal <corentin.vidal@epitech.eu>
2021-05-09 17:35:00 +0200

    Update: Player has no longer infinite stamina

--- sources/floor/floor_try_spawn.c
+++ sources/floor/floor_try_spawn.c
@@ -11,14 +11,21 @@
 #include "tiles_id.h"
 #include "entities/entity.h"
 #include "entities/slime.h"
+#include "entities/zombie.h"
+#include "random.h"
 #include <stdio.h>
 
 void floor_try_spawn(floor_t *self, int tries)
 {
     entity_t *new = 0;
+    int type;
 
     for (int i = 0; i < tries; i++) {
-        new = slime_create(1);
+        type = random_int(2);
+        if (type == 0)
+            new = slime_create(1);
+        if (type == 1)
+            new = zombie_create(1);
         if ((*new->funcs.start_pos)(new, self)) {
             floor_add(self, new);
         } else {
