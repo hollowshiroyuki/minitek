313c401e378f0dced0d6e0aeb8c4529d837a7ad7
hollow <35398794+hollowshiroyuki@users.noreply.github.com>
2023-08-18 22:08:34 +0200

    Add: Underground stairs generation

--- sources/level_gen/level_gen_create_underground_map/level_gen_create_underground_map.c
+++ sources/level_gen/level_gen_create_underground_map/level_gen_create_underground_map.c
@@ -25,6 +25,30 @@ static void destroy_noise(level_gen_t *n[11])
     }
 }
 
+static void generate_stairs(int *m, sfVector2i s)
+{
+    int count = 0;
+    sfVector2i p;
+    bool enclosed;
+
+    for (int i = 0; i < s.x * s.y / 100; i++) {
+        enclosed = true;
+        p.x = random_int(s.x - 20) + 10;
+        p.y = random_int(s.y - 20) + 10;
+        for (int y = p.y - 1; y <= p.y + 1; y++) {
+            for (int x = p.x - 1; x <= p.x + 1; x++) {
+                enclosed = (m[x + y * s.x] != T_ROCK) ? (false) : (enclosed);
+            }
+        }
+        if (!enclosed)
+            continue;
+        m[p.x + p.y * s.x] = T_STAIRDOWN;
+        count++;
+        if (count == 6)
+            return;
+    }
+}
+
 maps_t level_gen_create_underground_map(int w, int h, int depth)
 {
     level_gen_t *noises[11];
@@ -35,6 +59,7 @@ maps_t level_gen_create_underground_map(int w, int h, int depth)
     generate_noise(noises, (sfVector2i){w, h});
     generate_cave(noises, map, (sfVector2i){w, h}, depth);
     generate_ores(map, (sfVector2i){w, h}, depth);
+    generate_stairs(map, (sfVector2i){w, h});
     destroy_noise(noises);
     return ((maps_t){map, data});
 }
