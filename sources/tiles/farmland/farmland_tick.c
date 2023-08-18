b9c29d101c8944750207b9d31cfeee9c6de0e936
hollow <35398794+hollowshiroyuki@users.noreply.github.com>
2023-08-18 22:08:36 +0200

    Fix: Fixed farmland crash

--- sources/tiles/farmland/farmland_tick.c
+++ sources/tiles/farmland/farmland_tick.c
@@ -13,9 +13,8 @@
 #include "screen.h"
 #include <stdio.h>
 
-void farmland_tick(tile_t *self, floor_t *floor, sfVector2i pos)
+void farmland_tick(tile_t self, floor_t *floor, sfVector2i pos)
 {
-    printf("Reading %i, %i\n", pos.x, pos.y);
     int age = floor_get_data(floor, pos);
 
     if (age < 5)
